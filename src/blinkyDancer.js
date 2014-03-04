72f856be9544875c36a9b910f6fd3b52cee21b65
Hack Reactor Students <warrenng23@gmail.com>
2014-03-03 19:45:25 -0800

    blue and blinky are working

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,5 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
-  // this.$node = $('<span class="dancer"></span>');
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
--- src/init.js
+++ src/init.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data("data-dancer-maker-function-name");
-
+    var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
+    console.log(dancerMakerFunctionName);
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    console.log(dancerMakerFunction);
     // make a dancer with a random position
 
-    var dancer = new BlinkyDancer(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
